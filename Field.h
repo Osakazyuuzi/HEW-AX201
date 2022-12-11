367e5ee654f8c0fddf7122a8f5593ca4e33da8de
Osakazyuuzi <68203194+Osakazyuuzi@users.noreply.github.com>
2022-12-11 20:59:38 +0900

    読み込みするモデルを変更

--- Field.h
+++ Field.h
@@ -6,18 +6,18 @@
 class Field : public Object3D
 {
 public:
-	// ƒRƒ“ƒXƒgƒ‰ƒNƒ^
+	// ã‚³ãƒ³ã‚¹ãƒˆãƒ©ã‚¯ã‚¿
 	Field(std::string name, std::string tag) : Object3D(name, tag) {
-		// ƒvƒŒƒCƒ„[‚Ìƒ‚ƒfƒ‹‚ğƒ[ƒh‚·‚éi‰¼‚ÅƒLƒ…[ƒu‚ÌƒIƒuƒWƒFƒNƒgj
-		GetComponent<MeshRenderer>()->LoadModel("Assets/Model/Object/Cube.fbx", 1.0f);
-		// ƒRƒ“ƒ|[ƒlƒ“ƒg‚ğ’Ç‰Á
+		// ãƒ—ãƒ¬ã‚¤ãƒ¤ãƒ¼ã®ãƒ¢ãƒ‡ãƒ«ã‚’ãƒ­ãƒ¼ãƒ‰ã™ã‚‹ï¼ˆä»®ã§ã‚­ãƒ¥ãƒ¼ãƒ–ã®ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆï¼‰
+		GetComponent<MeshRenderer>()->LoadModel("Assets/Model/DemoCube/DemoCube.fbx", 1.0f);
+		// ã‚³ãƒ³ãƒãƒ¼ãƒãƒ³ãƒˆã‚’è¿½åŠ 
 		AddComponent<AABBCollider>();
-		// ƒIƒuƒWƒFƒNƒg‚Ì•Ó‚Ì’·‚³‚ğƒZƒbƒg
+		// ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆã®è¾ºã®é•·ã•ã‚’ã‚»ãƒƒãƒˆ
 		GetComponent<Transform>()->SetScale({ 20.0f, 1.0f, 20.0f });
-		// “–‚½‚è”»’è‚Ì•Ó‚Ì’·‚³‚ğƒZƒbƒg
+		// å½“ãŸã‚Šåˆ¤å®šã®è¾ºã®é•·ã•ã‚’ã‚»ãƒƒãƒˆ
 		GetComponent<AABBCollider>()->SetLen({ 20.0f,1.0f,20.0f });
 	}
-	// ƒfƒXƒgƒ‰ƒNƒ^
+	// ãƒ‡ã‚¹ãƒˆãƒ©ã‚¯ã‚¿
 	~Field() {}
 };
 
